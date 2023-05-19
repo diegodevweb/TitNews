ec824858797310e933c13061fdd6ed19dbe3b7e3
diegodevweb <diego.devwebb@gmail.com>
2023-05-19 19:45:06 -0300

    Refatoracao: Criacao de rotas get

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userService = require("../services/user.service");
+const mongoose = require("mongoose");
 
 const create = async (req, res) => {
     const {name, username, email, password, avatar} = req.body;
@@ -7,7 +8,7 @@ const create = async (req, res) => {
         res.status(400).send({message: "Preencha todos os campos!"});
     }
 
-    const user = await userService.create(req.body);
+    const user = await userService.createService(req.body);
 
     if(!user) {
         return res.status(400).send({message: "Erro ao criar usuario."});
@@ -25,4 +26,29 @@ const create = async (req, res) => {
     });
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
+    
+    if(users.length === 0) {
+        return res.status(400).send({ message: "Nao ha usuarios cadastrados."});
+    }
+
+    res.send(users);
+}
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({message: "ID invalido!"});
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if(!user) {
+        return res.status(400).send({message: "Usuario nao encontrado"});
+    }
+
+    res.send(user);
+}
+module.exports = { create, findAll, findById };
